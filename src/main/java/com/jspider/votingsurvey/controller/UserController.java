988e998a18a45cbd17ba0563a84bda3395d55a05
Mohsin <mdmohsinhaider6@gmail.com>
2025-02-17 17:51:37 +0530

    User service an controller

--- src/main/java/com/jspider/votingsurvey/controller/UserController.java
+++ src/main/java/com/jspider/votingsurvey/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.jspider.votingsurvey.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +15,7 @@ import com.jspider.votingsurvey.entity.User;
 import com.jspider.votingsurvey.services.UsersService;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/api/user")
 public class UserController {
 
 	@Autowired
@@ -20,5 +25,16 @@ public class UserController {
 	public User saveUserController(@RequestBody User user){
 		return service.saveUser(user);	
 	}
+	
+	@GetMapping
+	public List<User> getAllUserController(){
+		return service.getAllUsers();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<User> getUserByIdController(@PathVariable(name = "id") int id) {
+		return service.getUserById(id);
+	}
+	
 }
 
